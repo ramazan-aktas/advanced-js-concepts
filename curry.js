48b8b8908a52eac16244769cd5ffb95721132157
ramazan-aktas <111694852+ramazan-aktas@users.noreply.github.com>
2022-10-08 22:25:01 +0300

    Update curry.js
    typo

--- curry.js
+++ curry.js
@@ -1,6 +1,6 @@
 // Functions can return another functions. They can also be reconstructed into smaller functions...
 // Smaller functions that take one parameter each and pass through them. A little history: Alonso Church, who was a prominent figure in lambda calculus and considered as one of the founders of computer science... 
-// cam up with the idea in late 20s but he stripped it down to the (untyped) lambda calculus, circa 1936. Lambda calculus formalizes computation and is the basis of functional programming.
+// came up with the idea in late 20s but he stripped it down to the (untyped) lambda calculus, circa 1936. Lambda calculus formalizes computation and is the basis of functional programming.
 
 // Let's write a function with an arity of 3.
 function saluteGitHub(salute, git, hub) {
